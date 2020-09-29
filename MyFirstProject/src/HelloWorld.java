c5827bfbc629fb8591486b406ba77bd8a461f1e9
Ajdin Redzepovac <aredzepovac@softraysolutions.com>
2020-09-29 23:03:18 +0200

    start with java

---
+++ MyFirstProject/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	// this is a comment
+	
+	// Main is the class
+	// shortcut: type main + ctrl and space
+	public static void main(String[] args) {
+		// shortcut: type syso + ctrl and space
+		System.out.println("Hello World");
+	}
+}
