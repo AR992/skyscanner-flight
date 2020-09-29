f30aaf5d00d5b8412effe88aad2ea144563597d9
Ajdin Redzepovac <aredzepovac@softraysolutions.com>
2020-09-29 23:20:14 +0200

    java types and variables

---
+++ MyFirstProject/src/TypesAndVariables.java
@@ -0,0 +1,15 @@
+
+public class TypesAndVariables {
+	public static void main(String[] args) {
+		int myVariable; // declaring a variable
+		myVariable = 5; // initializing variable
+		
+		int myVar2 = 7; // declaring and initializing in one step
+		
+		String  hello = "Hello, ";
+		String name = "Joe";
+		
+		System.out.println(hello + name); // string concatenation
+	}
+
+}
