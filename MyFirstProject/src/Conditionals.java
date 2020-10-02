189e7fc08e6601ea32e134f8ace5c878b3c7b4a1
Ajdin Redzepovac <aredzepovac@softraysolutions.com>
2020-10-02 22:23:51 +0200

    methods, scope and conditionals in java

---
+++ MyFirstProject/src/Conditionals.java
@@ -0,0 +1,27 @@
+
+public class Conditionals {
+	public static void main(String[] args) {
+		
+		
+		// if (some expression that evaluate to true)
+		//     do something
+		
+		boolean sayHello = false;
+		boolean sayHey = true;
+		
+		// only the first statement to evaluate to true gets executed
+		if(sayHello) {
+			System.out.println("Hello");
+		} else if(sayHey) {
+			System.out.println("Hey");
+		} else {
+			System.out.println("Bye");
+		}
+		
+		// >, <, >=, <=, !=, ==  relation operators, also user true and false
+		
+		// && AND , || OR
+	
+	}
+
+}
---
+++ MyFirstProject/src/Methods.java
@@ -0,0 +1,35 @@
+
+public class Methods {
+	public static void main(String[] args) {
+		sayHelloWorld();
+		sayHelloWorld();
+		
+		sayHelloTo ("john");
+		sayHelloTo ("joe");
+		
+		int x = returnFive();		
+		System.out.println(x);
+		
+		int result = square(returnFive());
+		System.out.println(result);
+	}
+	
+	// this method both returns a value, and takes in data
+	static int square(int x) {
+		return x * x;
+	}
+	
+	//this method returns an int type with the value 5
+	static int returnFive() {
+		return 6;
+	}
+	
+	static void sayHelloTo (String name) {
+		System.out.println("Hello, " + name);
+	}
+	
+	static void sayHelloWorld () {
+		System.out.println("Hello world!");
+	}
+
+}
