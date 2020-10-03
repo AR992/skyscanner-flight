b3b13faa3743b33e3d74ef0a71cace4b44015567
Ajdin Redzepovac <aredzepovac@softraysolutions.com>
2020-10-03 21:20:58 +0200

    classes, objects and loops in java

---
+++ MyFirstProject/src/Loops.java
@@ -0,0 +1,32 @@
+
+public class Loops {
+	public static void main(String[] args) {
+				
+		// whileLoop();
+		// doWhileLoop();
+		forLoop();
+	}
+	
+	static void whileLoop() {
+		int counter = 10;
+		while(counter < 10) {
+			counter = counter + 1;	
+			System.out.println(counter);
+		}
+	}
+	
+	static void doWhileLoop() {
+		int counter = 10;
+		do {
+			counter = counter + 1;	
+			System.out.println(counter);
+		} while(counter < 10);
+	}
+	
+	static void forLoop() {
+		
+		for(int counter = 1; counter <= 10; counter = counter + 1) {
+			System.out.println(counter);
+		}
+	}
+}
---
+++ MyFirstProject/src/Objects.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Objects {
+	public static void main(String[] args) {
+		int number = 5;
+		
+		// create our window
+		JFrame window = new JFrame();    // create window variable that holds JFrame objects and we are creating a new JFrame objects
+		window.setTitle("My Window");	// we are calling methods on our JFrame objects to configure it	
+		window.setSize(800, 600);		
+		window.setVisible(true);
+		
+		JLabel label = new JLabel();	// create label variable that holds JLabel objects and we are creating a new JLabel objects	
+		label.setText("My Label");     // we are calling methods on our JLabel objects to set its text property
+		
+		window.add(label);            // we are calling the add method of our JFrame object to add our label to the window
+		
+		String  s = "hi";
+
+	}
+}
